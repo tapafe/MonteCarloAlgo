8eb9d0f6525dcb0577a70a5483737739033d5410
tapafe <arnaud.rigaut@viacesi.fr>
2020-06-02 11:03:37 +0200

    init

---
+++ MonteCarloAlgorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarloAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(7));
+            Console.Read();
+
+        }
+    }
+}
